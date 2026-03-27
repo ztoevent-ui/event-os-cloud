171bfd2522e546e7c9c23b00647b26f2fd6171ad
Tony <z.t.o.event@gmail.com>
2026-03-27 15:46:29 +0800

    Fix Turbopack syntax error: export default proxy function inside proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
     let response = NextResponse.next({
         request: {
             headers: request.headers,
